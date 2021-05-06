1a84e8630eba030d22a465c3a9ffb43aea7287fd
Vinícius Baptista de Mello (Frimesa) <viniciusbmello@gmail.com>
2021-05-06 17:45:48 -0300

    Componentizando Interface

--- src/@types/custom.d.ts
+++ src/@types/custom.d.ts
@@ -1,1 +1,29 @@
 declare module '*.svg'
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps{
+  movies: MovieProps[]
+  selectedGenre: GenreResponseProps
+}
+
+interface SideBarProps{
+  genres: GenreResponseProps[]
+  handleClickButton( id: number ): void
+  selectedGenreId: number
+}
